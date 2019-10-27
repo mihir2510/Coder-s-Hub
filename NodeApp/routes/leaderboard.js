b0d9b3b7d3d9fb424427f6612bcf4e198e5b404f
KaustubhDamania <kaustubh.damania@gmail.com>
2019-10-27 23:52:32 +0530

    Fixed leaderboard issues

--- NodeApp/routes/leaderboard.js
+++ NodeApp/routes/leaderboard.js
@@ -1,9 +1,7 @@
 var express = require('express');
 var router = express.Router();
+var leaderboard_controller = require('../controllers/leaderboard_controller');
 
-
-router.get('/leaderboard', function(req, res) {
-    res.redirect('/leaderboard');
-});
+router.get('/', leaderboard_controller.get_leaderboard);
 
 module.exports = router;
