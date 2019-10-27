aa6123254c6a7f90aa903cba3be381f5d98cc8cc
mihir2510 <mihirgada25@gmail.com>
2019-10-28 00:00:55 +0530

    home UI

--- NodeApp/routes/leaderboard.js
+++ NodeApp/routes/leaderboard.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 
-router.get('/leaderboard', function(req, res) {
+router.get('/', function(req, res) {
     res.redirect('/leaderboard');
 });
 
