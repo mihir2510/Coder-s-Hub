640ba4a9958c6c7f5a0b1eeeb8228fac080a4912
KaustubhDamania <kaustubh.damania@gmail.com>
2019-10-29 12:09:54 +0530

    Added schema stuff and leaderboard display

--- NodeApp/controllers/leaderboard_controller.js
+++ NodeApp/controllers/leaderboard_controller.js
@@ -1,4 +1,17 @@
-exports.get_leaderboard = function(req, res){
+var Account = require('../models/account');
+
+exports.get_leaderboard = async function(req, res){
     console.log('User is',req.user);
-    res.render('leaderboard',{user:req.user})
+    let users_list = await get_users();
+    console.log('Users are',users_list);
+    res.render('leaderboard',{user:req.user, users_list: users_list})
+}
+
+const get_users = async function(){
+    let arr = [];
+    await Account.find({},(err,res)=>{
+        // console.log(res);
+        arr = res;
+    });
+    return arr;
 }
